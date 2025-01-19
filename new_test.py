416313f1523af97abc43881cd7ad5459ce847eb1
Lewis <lewishotchkiss123@gmail.com>
2025-01-19 11:47:45 +0000

    added new structural metadata process method

--- new_test.py
+++ new_test.py
@@ -15,7 +15,7 @@ print(LABEL_MAPPING)
 ###
 
 ### Correlated Metadata Process Testing
-
+"""
 DATA = pd.read_csv("examples/NEW_TESTING_DATASET.csv")
 METADATA, LABEL_MAPPING, CORRELATION_MATRIX, MARGINALS = metadata_process(DATA, "id", "correlated") #, CORRELATION_MATRIX, MARGINALS
 
@@ -31,3 +31,13 @@ print()
 print(SYNTHETIC_DATA)
 
 SYNTHETIC_DATA.to_csv("output/NEW_TESTING_DATASET_SYNTHETIC_STRUCTURAL.csv")
+"""
+
+### New Structural Metadata Process Testing ###
+from synthopt.process.structural_metadata import process_structural_metadata
+DATA = pd.read_csv("examples/NEW_TESTING_DATASET.csv")
+print(DATA)
+#date_formats = ["%d/%m/%Y %H:%M:%S.%f", "%d/%m/%Y", "%H:%M:%S", "%d/%m/%Y %H:%M", "%Y-%m-%d"]
+date_formats = None
+METADATA = process_structural_metadata(DATA, date_formats, "Test")
+print(METADATA)
