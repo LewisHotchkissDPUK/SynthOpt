db94a361afb12d014f64de71d14b41766bfb2845
Lewis Hotchkiss <lewis.hotchkiss@chi.swan.ac.uk>
2025-01-23 09:48:11 +0000

    fixed multi table stats synth

--- new_test.py
+++ new_test.py
@@ -58,7 +58,7 @@ SYNTHETIC_DATA['Healthcare'].to_csv("output/new_structural_healthcare.csv")
 
 ### New Statistical Metadata Process Testing ###
 from synthopt.process.statistical_metadata import process_statistical_metadata
-STATS_METADATA = process_statistical_metadata(DATA, date_formats, "Test")
+STATS_METADATA = process_statistical_metadata(DATASETS, date_formats, "Test")
 print("STATS METADATA")
 print(STATS_METADATA)
 #STATS_METADATA['Test'].to_csv("output/NEW_TESTING_DATASET_STATS_METADATA.csv")
@@ -67,6 +67,7 @@ print(STATS_METADATA)
 ### New Statistical Generation Testing ###
 from synthopt.generate.statistical_synthetic_data import generate_statistical_synthetic_data
 STATS_SYNTHETIC_DATA = generate_statistical_synthetic_data(STATS_METADATA, num_records=1000, identifier_column="id")
-print(STATS_SYNTHETIC_DATA)
+print(STATS_SYNTHETIC_DATA['Test'])
+print(STATS_SYNTHETIC_DATA['Healthcare'])
 #STATS_SYNTHETIC_DATA['Test'].to_csv("output/NEW_TESTING_DATASET_SYNTHETIC_STATS.csv")
 #STATS_SYNTHETIC_DATA['Healthcare'].to_csv("output/HEALTHCARE_SYNTHETIC_STATS.csv")
--- synthopt/process/statistical_metadata.py
+++ synthopt/process/statistical_metadata.py
@@ -5,9 +5,9 @@ from tqdm import tqdm
 
 def process_statistical_metadata(data, datetime_formats=None, table_name=None):
     if isinstance(data, dict):
-        all_metadata = {}
+        all_metadata = []
         for key, dataset in data.items():
-            metadata, cleaned_data = process_structural_metadata(dataset, datetime_formats, table_name, return_data=True)
+            metadata, cleaned_data = process_structural_metadata(dataset, datetime_formats, key, return_data=True)
             metadata.index = metadata['variable_name']
 
             numerical_cleaned_data = cleaned_data.select_dtypes(include=['number'])
@@ -15,8 +15,9 @@ def process_statistical_metadata(data, datetime_formats=None, table_name=None):
             new_metadata = metadata.join(best_fit_metadata)
             new_metadata = new_metadata.reset_index(drop=True)
 
-            all_metadata[key] = new_metadata
-        return all_metadata
+            all_metadata.append(new_metadata)
+        final_combined_metadata = pd.concat(all_metadata, ignore_index=True)
+        return final_combined_metadata
     else:
         metadata, cleaned_data = process_structural_metadata(data, datetime_formats, table_name, return_data=True)
         metadata.index = metadata['variable_name']
